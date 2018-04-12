e3a1a7d7885d5aaf4569988e4e533aba404a8b46
Kishan B <kishanb_uk@hotmail.com>
2018-04-12 14:16:43 +0100

    added test names and artist to record spec

---
+++ specs/record_spec.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+const Journey = require('../record.js');
+
+describe('Record', function() {
+
+  let artist1
+
+  beforeEach(function() {
+
+    artist = new Artist('Papa Darren', 'Down the rabbit hole', 'hip-hop', 4.99)
+
+  });
+
+  it('should have a Artist', function() {
+
+  });
+
+  it('should have a Title', function() {
+
+  });
+
+  it('should have a Genre', function() {
+
+  });
+
+  it('should have a Price', function() {
+
+  });
+});
