447496db17382751e4d888fc50a9876ab18ae261
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2018-04-12 15:41:13 +0100

    Added function to customer to get total value of collection

--- customer.js
+++ customer.js
@@ -8,11 +8,19 @@ const Customer = function (name, cash) {
 Customer.prototype.buyRecord = function (record) {
   if (this.cash >= record.price) {
     this.cash -= record.price;
+    this.recordCollection.push(record);
   }
 };
 
 Customer.prototype.sellRecord = function (record) {
   this.cash += record.price;
+};
+
+Customer.prototype.getValueOfCollection = function () {
+  const reducer = (runningTotal, record) => {
+    return runningTotal + record.price;
+  }
+  return this.recordCollection.reduce(reducer, 0);
 };
 
 module.exports = Customer;
--- specs/customer_spec.js
+++ specs/customer_spec.js
@@ -5,11 +5,13 @@ const Record = require('../record.js')
 describe('Customer', function () {
 
   let customer;
-  let record;
+  let record1;
+  let record2;
 
   beforeEach(function () {
-    customer = new Customer('Finn', 10.00)
-    record = new Record("John's Merry Band", "CodeClan United", 'death metal', 5.50);
+    customer = new Customer('Finn', 12.00)
+    record1 = new Record("John's Merry Band", "CodeClan United", 'death metal', 5.50);
+    record2 = new Record("E19 Crew", "Cheese and Wine", 'easy listening', 15.00);
   });
 
   it('should have a name', function () {
@@ -17,7 +19,7 @@ describe('Customer', function () {
   });
 
   it('should have cash', function () {
-    assert.strictEqual(customer.cash, 10.00);
+    assert.strictEqual(customer.cash, 12.00);
   });
 
   it('should start with an empty record collection', function () {
@@ -25,23 +27,30 @@ describe('Customer', function () {
   });
 
   it('should have their money decrease when buying a record', function () {
-    customer.buyRecord(record);
-    assert.strictEqual(customer.cash, 4.50);
+    customer.buyRecord(record1);
+    assert.strictEqual(customer.cash, 6.50);
   });
 
   it('should have their money increase when selling a record', function () {
-    customer.sellRecord(record);
-    assert.strictEqual(customer.cash, 15.50);
+    customer.sellRecord(record1);
+    assert.strictEqual(customer.cash, 17.50);
   });
 
   it('should be able to buy a record (having enough money)', function () {
-    customer.buyRecord(record);
-    assert.strictEqual(customer.cash, 4.50);
+    customer.buyRecord(record1);
+    assert.strictEqual(customer.cash, 6.50);
+    assert.deepStrictEqual(customer.recordCollection, [record1]);
   });
 
   it('should not be able to buy a record (having not enough money)', function () {
-    record.price = 11.00;
-    customer.buyRecord(record);
-    assert.strictEqual(customer.cash, 10.00);
+    customer.buyRecord(record2);
+    assert.strictEqual(customer.cash, 12.00);
+    assert.deepStrictEqual(customer.recordCollection, []);
   });
+
+  it('should be able to get the value of their collection', function () {
+    customer.buyRecord(record1);
+    customer.buyRecord(record1);
+    assert.strictEqual(customer.getValueOfCollection(), 11.00);
+  })
 });
