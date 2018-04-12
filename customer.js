6c6bcb892dbe66590034d7ac4612cc6ab38370ee
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2018-04-12 15:23:16 +0100

    Added buy and sell methods to customer, added check so customer cannot buy if not enough money

--- customer.js
+++ customer.js
@@ -4,4 +4,15 @@ const Customer = function (name, cash) {
   this.recordCollection = [];
 };
 
+
+Customer.prototype.buyRecord = function (record) {
+  if (this.cash >= record.price) {
+    this.cash -= record.price;
+  }
+};
+
+Customer.prototype.sellRecord = function (record) {
+  this.cash += record.price;
+};
+
 module.exports = Customer;
--- specs/customer_spec.js
+++ specs/customer_spec.js
@@ -1,12 +1,15 @@
 const assert = require('assert');
 const Customer = require('../customer.js');
+const Record = require('../record.js')
 
 describe('Customer', function () {
 
   let customer;
+  let record;
 
   beforeEach(function () {
     customer = new Customer('Finn', 10.00)
+    record = new Record("John's Merry Band", "CodeClan United", 'death metal', 5.50);
   });
 
   it('should have a name', function () {
@@ -19,5 +22,26 @@ describe('Customer', function () {
 
   it('should start with an empty record collection', function () {
     assert.deepStrictEqual(customer.recordCollection, []);
+  });
+
+  it('should have their money decrease when buying a record', function () {
+    customer.buyRecord(record);
+    assert.strictEqual(customer.cash, 4.50);
+  });
+
+  it('should have their money increase when selling a record', function () {
+    customer.sellRecord(record);
+    assert.strictEqual(customer.cash, 15.50);
+  });
+
+  it('should be able to buy a record (having enough money)', function () {
+    customer.buyRecord(record);
+    assert.strictEqual(customer.cash, 4.50);
+  });
+
+  it('should not be able to buy a record (having not enough money)', function () {
+    record.price = 11.00;
+    customer.buyRecord(record);
+    assert.strictEqual(customer.cash, 10.00);
   });
 });
